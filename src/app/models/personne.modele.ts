a5583df64a3a9760d65910df89f3b8248a404009
BenSira99 <ligbandrohbensira@gmail.com>
2026-03-05 15:07:13 +0000

    feat(personne): ajout des getters et setters au modèle Personne

--- src/app/models/personne.modele.ts
+++ src/app/models/personne.modele.ts
@@ -1,9 +1,37 @@
 export class Personne {
-    constructor(
-        public nom: string,
-        public prenom: string,
-        public numero: string
-    ) { }
+    private _nom: string;
+    private _prenom: string;
+    private _numero: string;
+
+    constructor(nom: string, prenom: string, numero: string) {
+        this._nom = nom;
+        this._prenom = prenom;
+        this._numero = numero;
+    }
+
+    get nom(): string {
+        return this._nom;
+    }
+
+    set nom(valeur: string) {
+        this._nom = valeur;
+    }
+
+    get prenom(): string {
+        return this._prenom;
+    }
+
+    set prenom(valeur: string) {
+        this._prenom = valeur;
+    }
+
+    get numero(): string {
+        return this._numero;
+    }
+
+    set numero(valeur: string) {
+        this._numero = valeur;
+    }
 
     obtenirDetails(): string {
         return `${this.prenom} ${this.nom} (N° ${this.numero})`;
